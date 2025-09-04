53335128172fa38a7367b14f16db050363bc70b9
Xxeet7 <2good4u991@gmail.com>
2025-09-04 08:57:50 +0700

    refactor: cleaning up

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -9,10 +9,49 @@ return {
   event = "VeryLazy",
   opts = {
     win = {
-      no_overlap = false,
+      no_overlap = true,
     },
     icons = {
       group = "",
+    },
+  },
+  config = function()
+    wk = require "which-key"
+    wk.add {
+      {
+        "<leader>e",
+        desc = "Explorer (Root)",
+        icon = { icon = "", color = "yellow" },
+      },
+      {
+        "<leader>E",
+        desc = "Explorer (CWD)",
+        icon = { icon = "", color = "yellow" },
+      },
+      {
+        "<leader>d",
+        desc = "Delete without yanking",
+        icon = { icon = "󰗨", color = "red" },
+      },
+      {
+        "<leader>y",
+        desc = "yazi",
+        icon = { icon = "󰇥", color = "yellow" },
+      },
+      {
+        "<leader>s",
+        desc = "split",
+        icon = { icon = "", color = "blue" },
+      },
+      { "<leader>o", group = "open", icon = { icon = "" } },
+      { "<leader>c", group = "code", icon = { icon = "", color = "azure" } },
+      { "<leader>f", group = "telescope", icon = { icon = "", color = "green" } },
+      { "<leader>a", group = "AI", icon = { icon = "", color = "azure" } },
+      { "<leader>t", group = "toggle", icon = { icon = "", color = "yellow" } },
+      { "<leader>l", group = "LSP", icon = { icon = "󰒍", color = "blue" } },
+      { "<leader>w", group = "Workspace", icon = { icon = "", color = "blue" } },
+      { "<leader>r", group = "Variables", icon = { icon = "", color = "red" } },
+      { "<leader>v", group = "Vim", icon = { icon = "", color = "green" } },
     }
-  }
+  end,
 }
