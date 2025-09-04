224d45a01a2dae7a834c1a43e9b255bbbb615479
Xxeet7 <2good4u991@gmail.com>
2025-09-04 12:08:58 +0700

    style(nvim-tree): change nvim-tree to buffer on right and add keymap for split

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,6 +3,21 @@
 -- ABOUT : A file explorer tree for neovim written in lua
 -- LINKS : https://github.com/nvim-tree/nvim-tree.lua
 -- ================================================================================================
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', '<leader>sv', api.node.open.vertical, opts('Open file in vertical split'))
+  vim.keymap.set('n', '<leader>sh', api.node.open.horizontal, opts('Open file in horizontal split'))
+end
 
 return {
   {
@@ -10,6 +25,7 @@ return {
     -- lazy = false,
     event = "VeryLazy",
     opts = {
+      on_attach = my_on_attach,
       -- sync_root_with_cwd = true,
       -- respect_buf_cwd = true,
       -- update_focused_file = {
@@ -20,16 +36,16 @@ return {
         -- number = true,
         -- relativenumber = true,
         side = "right",
-        width = 35,
-        float = {
-          enable = true,
-          open_win_config = {
-            width = 35,
-            height = 26,
-            row = 1,
-            col = 90,
-          },
-        },
+        width = 45,
+        -- float = {
+        --   enable = true,
+        --   open_win_config = {
+        --     width = 35,
+        --     height = 26,
+        --     row = 1,
+        --     col = 90,
+        --   },
+        -- },
       },
       actions = {
         open_file = {
