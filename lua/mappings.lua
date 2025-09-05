27badf62c781cc39c76706dff561640b2e0dd6ca
Xxeet7 <2good4u991@gmail.com>
2025-09-05 12:52:11 +0700

    feat: add menu for right click

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -129,3 +129,16 @@ map("n", "<C-Right>", "<Cmd>vertical resize -2<CR>", { desc = "Increase window w
 map({ "n", "t" }, "<A-g>", function()
   require("nvchad.term").toggle { pos = "float", id = "geminiterm", cmd = "gemini" }
 end, { desc = "toggle gemini term" })
+
+-- Nvmenu
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
